54d63d52e55c87d0b801f0f5cc54112803b9a93e
kimi spencer <kimispencer@gmail.com>
2019-04-30 16:10:18 -0700

    example

--- docs/Examples/Border.example.purs
+++ docs/Examples/Border.example.purs
@@ -9,21 +9,14 @@ import React.Basic (Component, JSX, createComponent, makeStateless)
 import React.Basic.DOM (css)
 import React.Basic.DOM as R
 
-component :: Component Unit
-component = createComponent "BorderExample"
-
 docs :: JSX
-docs = unit # makeStateless component render
-  where
-    render _ =
-      column_
-        [ example
-            $ border
-                { children:
-                    [ R.div
-                      { children: R.text "bordered element"
-                      , style: R.css { padding: "12px" }
-                      }
-                    ]
-                }
-        ]
+docs =
+  column_
+    [ example
+        $ border
+          [ R.div
+              { children: [ R.text "border element" ]
+              , style: R.css { padding: "12px" }
+              }
+          ]
+    ]
