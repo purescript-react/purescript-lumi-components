9e02295d6240a91270eed3c6a78ef1ca3ee15d58
Madeline Trotter <spicydonuts@gmail.com>
2019-08-01 10:36:21 -0700

    Fix clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const Webpack = require("webpack");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ResourceHintWebpackPlugin = require("resource-hints-webpack-plugin");
