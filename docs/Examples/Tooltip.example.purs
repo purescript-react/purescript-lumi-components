3257da07665eb87690bfd267fd27413abdbfe744
kimi spencer <kimispencer@gmail.com>
2019-10-11 15:14:02 -0400

    Revert "Array JSX"
    This reverts commit c7ba2068590e9e066279671a5ad10fe163340ca1.

--- docs/Examples/Tooltip.example.purs
+++ docs/Examples/Tooltip.example.purs
@@ -9,6 +9,7 @@ import Lumi.Components.Column (column_)
 import Lumi.Components.Link as Link
 import Lumi.Components.Row (row_)
 import Lumi.Components.Text (body_)
+import Lumi.Components.Text as T
 import Lumi.Components.Example (example)
 import Lumi.Components.Tooltip (tooltip)
 import React.Basic (JSX)
@@ -22,7 +23,7 @@ docs =
         $ tooltip
             { variant: "basic"
             , style: R.css {}
-            , text: [ R.text "Lorem ipsum" ]
+            , text: R.text "Lorem ipsum"
             , content: body_ "Basic example"
             , size: toNullable Nothing
             }
@@ -31,7 +32,7 @@ docs =
         $ tooltip
             { variant: "top"
             , style: R.css {}
-            , text: [ R.text "Lorem ipsum" ]
+            , text: R.text "Lorem ipsum"
             , content: body_ "Top example"
             , size: toNullable Nothing
             }
@@ -40,7 +41,7 @@ docs =
         $ tooltip
             { variant: "bottom"
             , style: R.css {}
-            , text: [ R.text "Lorem ipsum" ]
+            , text: R.text "Lorem ipsum"
             , content: body_ "Bottom example"
             , size: toNullable Nothing
             }
@@ -49,7 +50,7 @@ docs =
         $ tooltip
             { variant: "left"
             , style: R.css {}
-            , text: [ R.text "Lorem ipsum" ]
+            , text: R.text "Lorem ipsum"
             , content: body_ "Left example"
             , size: toNullable Nothing
             }
@@ -60,7 +61,7 @@ docs =
               , tooltip
                   { variant: "top"
                   , style: R.css { padding: "0 2px", textDecoration: "underline" }
-                  , text: [ R.text "Lorem ipsum" ]
+                  , text: R.text "Lorem ipsum"
                   , content: body_ "here"
                   , size: toNullable Nothing
                   }
@@ -73,7 +74,7 @@ docs =
               , tooltip
                   { variant: "top"
                   , style: R.css { padding: "0 2px", textDecoration: "underline" }
-                  , text: [ R.text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor libero non libero consequat, at iaculis diam venenatis. Donec nec porttitor tellus." ]
+                  , text: R.text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor libero non libero consequat, at iaculis diam venenatis. Donec nec porttitor tellus."
                   , content: body_ "here"
                   , size: toNullable $ Just $ Large
                   }
@@ -87,14 +88,18 @@ docs =
                   { variant: "top"
                   , style: R.css { padding: "0 2px", textDecoration: "underline" }
                   , text:
-                      [ R.text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor libero non libero consequat, at iaculis diam venenatis. Donec nec porttitor tellus "
-                      , Link.link Link.defaults
-                          { href = URL "#/link"
-                          , text = R.text "click here"
-                          , target = Just "_blank"
-                          }
-                      , R.text "."
-                      ]
+                      T.text T.body
+                        { style = R.css {}
+                        , children =
+                            [ R.text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor libero non libero consequat, at iaculis diam venenatis. Donec nec porttitor tellus "
+                            , Link.link Link.defaults
+                                { href = URL "#/link"
+                                , text = R.text "click here"
+                                , target = Just "_blank"
+                                }
+                            , R.text "."
+                            ]
+                        }
                   , content: body_ "here"
                   , size: toNullable $ Just $ Large
                   }
