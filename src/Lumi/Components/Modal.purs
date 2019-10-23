6f09445c20dfe79226f1471b22d8a0d2881f5d81
kimi spencer <kimispencer@gmail.com>
2019-10-23 12:54:28 -0400

    code review

--- src/Lumi/Components/Modal.purs
+++ src/Lumi/Components/Modal.purs
@@ -435,6 +435,8 @@ styles = jss
                       { "@media (max-width: 860px)":
                           { position: "fixed"
                           , top: "0"
+                          , right: "0"
+                          , bottom: "0"
                           , left: "0"
                           , height: "100%"
                           , width: "100vw"
