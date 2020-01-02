92aaebe39357c688098e5c7a8ae43fdc6898bd1d
Madeline Trotter <spicydonuts@gmail.com>
2020-01-02 13:55:57 -0600

    Expose ScrollObserver as a hook (#115)

---
+++ src/Lumi/Components2/ScrollObserver.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// Walks up the DOM tree starting at the given node to find
+// the first parent with a scroll bar (including auto, which
+// on some devices hides the scroll bar until hovered).
+exports.getScrollParent = node => () => {
+  const isElement = node instanceof HTMLElement;
+  const computedStyle = isElement && window.getComputedStyle(node);
+  const overflowY = computedStyle && computedStyle.overflowY;
+  const overflowX = computedStyle && computedStyle.overflowX;
+  const isScrollable =
+    (overflowY &&
+      !(overflowY.includes("visible") || overflowY.includes("hidden"))) ||
+    (overflowX &&
+      !(overflowX.includes("visible") || overflowX.includes("hidden")));
+
+  if (!node) {
+    return document.body;
+  } else if (isScrollable && node.scrollHeight >= node.clientHeight) {
+    return node;
+  } else {
+    return getScrollParent(node.parentNode);
+  }
+};
+
+exports.addPassiveEventListener = type => listener => capture => target => () =>
+  target.addEventListener(type, listener, { passive: true, capture });
+
+exports.removePassiveEventListener = type => listener => capture => target => () =>
+  target.removeEventListener(type, listener, { passive: true, capture });
