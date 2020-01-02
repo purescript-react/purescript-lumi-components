a5884f8da4bda191958093320c49ceae1054fc92
Madeline Trotter <spicydonuts@gmail.com>
2020-01-02 10:34:57 -0800

    Add clip example description

--- docs/Examples2/Clip.example.purs
+++ docs/Examples2/Clip.example.purs
@@ -14,10 +14,12 @@ docs =
   lumiElement box
     _
       { content =
-        [ p_ "..."
+        [ p_ "The Clip component wraps the provided content with a grey border and a \"Copy\" button, which copies the text content into the system clipboard."
+        , p_ "If clipboard access is not allowed or not supported the text will be left highlighted, allowing the user to press ctrl+c manually. Only the plain text content is copied, not the HTML."
         , vspace S24
         , example
             $ lumiElement clip
             $ _ { content = [ body_ "someone@email.com" ] }
+        , p_ "The Clip behavior is also available as a React hook."
         ]
       }
