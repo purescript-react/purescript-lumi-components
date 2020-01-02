4eaf3a76a791262800c8b0504edcfc6cbe9610b0
Madeline Trotter <spicydonuts@gmail.com>
2020-01-02 13:53:46 -0600

    Add Clip component (#116)
    * Add Clip component
    
    * Clip hook and browser support
    
    * Add clip example description

---
+++ src/Lumi/Components2/Clip.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const selectNodeContents = node => {
+  const range = document.createRange();
+  range.selectNodeContents(node);
+  var sel = window.getSelection();
+  sel.removeAllRanges();
+  sel.addRange(range);
+  return sel;
+};
+
+exports.copyNodeContents = (success, failure, node) => {
+  try {
+    const sel = selectNodeContents(node);
+    if (window.navigator.clipboard != null) {
+      window.navigator.clipboard
+        .writeText(sel.toString())
+        .then(() => {
+          sel.removeAllRanges();
+        })
+        .then(success, failure);
+      return;
+    } else {
+      const copyResult = window.document.execCommand("copy");
+      if (copyResult) {
+        sel.removeAllRanges();
+        return success();
+      } else {
+        return failure(new Error("Failed to copy"));
+      }
+    }
+  } catch (e) {
+    failure(e);
+  }
+};
---
+++ src/Lumi/Styles/Clip.purs
@@ -0,0 +1,21 @@
+module Lumi.Styles.Clip where
+
+import Prelude
+import Lumi.Components (PropsModifier)
+import Lumi.Styles (styleModifier)
+import Lumi.Styles.Border (_round, border)
+import Lumi.Styles.Box (FlexAlign(..), _justify, _row)
+import Lumi.Styles.Theme (LumiTheme(..))
+import React.Basic.Emotion (color, css)
+
+clip :: forall props. PropsModifier props
+clip =
+  border
+    >>> _round
+    >>> _row
+    >>> _justify SpaceBetween
+    >>> styleModifier \(LumiTheme { colors }) ->
+        css
+          { borderColor: color colors.black5
+          , backgroundColor: color colors.black5
+          }
