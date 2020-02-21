a51fa6b5149cee004b95f13d01cb93b04c084fda
kimi spencer <kimispencer@gmail.com>
2020-02-20 18:05:12 -0600

    cleanup imports

--- src/Lumi/Styles/Slat.purs
+++ src/Lumi/Styles/Slat.purs
@@ -6,13 +6,11 @@ module Lumi.Styles.Slat
 import Prelude
 
 import Lumi.Components (PropsModifier)
-import Lumi.Styles (styleModifier, styleModifier_)
-import Lumi.Styles.Box (FlexAlign(..), _align, _interactive, _justify, _row)
-import Lumi.Styles.Box as Box
+import Lumi.Styles (styleModifier_)
+import Lumi.Styles.Box (FlexAlign(..), _align, _justify, _row)
 import Lumi.Styles.Border (border)
 import Lumi.Styles.Border hiding (border) as Border
-import Lumi.Styles.Theme (LumiTheme(..))
-import React.Basic.Emotion (color, css, str, nested, unset)
+import React.Basic.Emotion (css, str, unset)
 
 slat :: forall props. PropsModifier props
 slat =
