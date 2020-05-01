a2f11a1c020ce9ae8f55812fcbe4f070b28c84a0
Arthur Xavier <arthur.xavierx@gmail.com>
2020-05-01 14:13:48 -0300

    Add combinators for using an array of styles as a prop modifier

--- src/Lumi/Styles.purs
+++ src/Lumi/Styles.purs
@@ -7,9 +7,11 @@ module Lumi.Styles
   ) where
 
 import Prelude
+
+import Data.Foldable (fold)
 import Lumi.Components (PropsModifier, propsModifier)
 import Lumi.Styles.Theme (LumiTheme)
-import React.Basic.Emotion hiding (element, style) as Emotion
+import React.Basic.Emotion hiding (element,style) as Emotion
 
 type StyleModifier = forall props. PropsModifier props
 
@@ -20,6 +22,14 @@ style f = propsModifier \props -> props { css = props.css <> f }
 -- | Lift a static set of styles into a `StyleModifier` for composition with other modifiers.
 style_ :: Emotion.Style -> StyleModifier
 style_ = style <<< const
+
+-- | Lift an array of themed styles into a `StyleModifier` for composition with other modifiers.
+styles :: Array (LumiTheme -> Emotion.Style) -> StyleModifier
+styles = style <<< fold
+
+-- | Lift an array of static styles into a `StyleModifier` for composition with other modifiers.
+styles_ :: Array Emotion.Style -> StyleModifier
+styles_ = style_ <<< fold
 
 -- | Flatten a `PropsModifier` and extract the Emotion styles for use with `React.Basic.Emotion.element`.
 -- | This function is mainly used inside component implementations where the `LumiComponent` boundary
