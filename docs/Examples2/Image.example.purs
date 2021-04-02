7528dfc3df82cb9f93d46f57ff43e2b488bc4825
Madeline Trotter <spicydonuts@gmail.com>
2021-04-02 09:24:56 -0700

    Fix css; src -> content; int -> px

--- docs/Examples2/Image.example.purs
+++ docs/Examples2/Image.example.purs
@@ -3,6 +3,7 @@ module Lumi.Components2.Examples.Image where
 import Prelude
 
 import Data.Array as Array
+import Lumi.Components (($$$))
 import Lumi.Components.Example (example)
 import Lumi.Components.Spacing (Space(..), vspace)
 import Lumi.Components.Text (h4_)
@@ -16,44 +17,44 @@ docs =
     [ h4_ "Thumbnail (always have a square aspect ratio)"
     , example
         $ Image.thumbnail
-        $ _ { src = "http://via.placeholder.com/640x360"  }
+        $$$ "http://via.placeholder.com/640x360"
     , h4_ "Thumbnail + resize 48px"
     , example
         $ Image.thumbnail
         $ Image.resizeSquare 80
-        $ _ { src = flexo }
+        $$$ flexo
     , h4_ "Thumbnail + small"
     , example
         $ Image.thumbnail
         $ Image.small
-        $ _ { src = flexo }
+        $$$ flexo
     , h4_ "Thumbnail + medium"
     , example
         $ Image.thumbnail
         $ Image.medium
-        $ _ { src = flexo }
+        $$$ flexo
     , h4_ "Thumbnail + large"
     , example
         $ Image.thumbnail
         $ Image.large
-        $ _ { src = flexo }
+        $$$ flexo
     , h4_ "Thumbnail + extra large"
     , example
         $ Image.thumbnail
         $ Image.extraLarge
-        $ _ { src = flexo }
+        $$$ flexo
     , h4_ "Thumbnail + round"
     , example
         $ Image.thumbnail
         $ Image.round
-        $ _ { src = flexo }
+        $$$ flexo
     , h4_ "Image (no size restrictions)"
     , example
         $ Image.image
-        $ _ { src = "http://via.placeholder.com/640x360" }
+        $$$ "http://via.placeholder.com/640x360"
     , h4_ "Image + resize { width: 90px, height: 50px } "
     , example
         $ Image.image
         $ Image.resize { width: 90, height: 50 }
-        $ _ { src = "http://via.placeholder.com/640x360" }
+        $$$ "http://via.placeholder.com/640x360"
     ]
--- src/Lumi/Components2/Image.purs
+++ src/Lumi/Components2/Image.purs
@@ -20,7 +20,6 @@ import Data.String as String
 import Data.Tuple.Nested ((/\))
 import Effect.Unsafe (unsafePerformEffect)
 import Lumi.Components (LumiComponent, PropsModifier, lumiComponent, ($$$))
-import Lumi.Components.Loader (loader)
 import Lumi.Components.Svg (placeholderSvg)
 import Lumi.Components2.Box as Box
 import Lumi.Styles (Style, StyleModifier, style, style_, toCSS)
@@ -38,7 +37,7 @@ data Image = Image
 
 type ImageProps
   = ( component :: Image
-    , src :: String
+    , content :: String
     , placeholder :: Maybe JSX
     )
 
@@ -53,7 +52,7 @@ image =
       pure
         $ E.element R.div'
             { children:
-                [ if String.null props.src
+                [ if String.null props.content
                     then fromMaybe placeholderSvg props.placeholder
                     else
                       Box.column
@@ -65,7 +64,7 @@ image =
                             -- @TODO confirming with design
                             -- $ loader { style: R.css { width: "20px", height: "20px", borderWidth: "2px" }, testId: Nullable.toNullable Nothing }
                         , E.element R.img'
-                            { src: props.src
+                            { src: props.content
                             , className: ""
                             , css: E.css { maxWidth: E.percent 100.0 }
                             , onLoad: handler_ $ setLoaded true
@@ -78,16 +77,16 @@ image =
     where
       defaults =
         { component: Image
-        , src: ""
+        , content: ""
         , placeholder: Nothing
         }
 
       defaultImageStyle :: LumiTheme -> Style
       defaultImageStyle theme@(LumiTheme { colors }) =
           E.css
-            { boxSizing: E.str "border-box"
-            , overflow: E.str "hidden"
-            , display: E.str "flex"
+            { boxSizing: E.borderBox
+            , overflow: E.hidden
+            , display: E.flex
             , border: E.str "1px solid"
             , borderColor: E.color colors.black4
             }
@@ -96,7 +95,7 @@ data Thumbnail = Thumbnail
 
 type ThumbnailProps
   = ( component :: Thumbnail
-    , src :: String
+    , content :: String
     , placeholder :: Maybe JSX
     )
 
@@ -105,21 +104,17 @@ thumbnail :: LumiComponent ThumbnailProps
 thumbnail =
   unsafePerformEffect do
     lumiComponent "Thumbnail" defaults \props -> Hooks.do
-      theme <- useTheme
       pure
         $ image
-        $ _ { src = props.src
+        $ _ { content = props.content
             , placeholder = props.placeholder
-            -- @TODO get this to compile
-            -- we should be setting the default square size on thumbnails
-            -- , css = theme # toCSS defaultSize <> props.css
-            , css = props.css
+            , css = toCSS defaultSize <> props.css
             }
 
       where
         defaults =
           { component: Thumbnail
-          , src: ""
+          , content: ""
           , placeholder: Nothing
           }
 
@@ -132,16 +127,16 @@ resize :: { width :: Int, height :: Int } -> ImageModifier Image
 resize props =
   style \(LumiTheme theme) ->
     E.css
-      { width: E.int props.width
-      , height: E.int props.height
+      { width: E.px props.width
+      , height: E.px props.height
       }
 
 -- | restricted to only Thumbnail
 defaultSize :: StyleModifier
 defaultSize =
   style_ $ E.css
-      { width: E.int 30
-      , height: E.int 30
+      { width: E.px 30
+      , height: E.px 30
       }
 
 round :: ImageModifier Thumbnail
@@ -155,16 +150,16 @@ resizeSquare :: Int -> ImageModifier Thumbnail
 resizeSquare size =
   style \(LumiTheme theme) ->
     E.css
-      { width: E.int size
-      , height: E.int size
+      { width: E.px size
+      , height: E.px size
       }
 
 small :: ImageModifier Thumbnail
 small =
   style_
     $ E.css
-      { width: E.int 24
-      , height: E.int 24
+      { width: E.px 24
+      , height: E.px 24
       }
 
 medium :: ImageModifier Thumbnail
@@ -174,16 +169,16 @@ large :: ImageModifier Thumbnail
 large =
   style_
     $ E.css
-      { width: E.int 36
-      , height: E.int 36
+      { width: E.px 36
+      , height: E.px 36
       }
 
 extraLarge :: ImageModifier Thumbnail
 extraLarge =
   style_
     $ E.css
-      { width: E.int 140
-      , height: E.int 140
+      { width: E.px 140
+      , height: E.px 140
       }
 
 
